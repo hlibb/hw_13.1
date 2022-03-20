4aeb618581892cd888feee4e53b40987641c8c95
Соня <Sonya@iMac-Andrej.local>
2022-03-20 19:20:51 +0200

    Initial commit

---
+++ src/com/company/Logger.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Logger {
+    public void info(String msg);
+    //public void info(String msg, Object o);
+    public void error(String msg);
+    //public void error(String msg, Object o);
+}
---
+++ src/com/company/Logger1.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Logger1 implements Logger {
+    @Override
+    public void info(String msg) {
+        System.out.println();
+    }
+
+    @Override
+    public void error(String msg) {
+        System.out.println();
+    }
+}
